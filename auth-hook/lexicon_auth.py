7e2514afb8e8568852cf161d2a60f3f029422c01
AJ Slater <aj@slater.net>
2022-04-01 12:18:01 -0700

    better logging

--- auth-hook/lexicon_auth.py
+++ auth-hook/lexicon_auth.py
@@ -8,10 +8,14 @@ But these can be overridden as when used in the flask server
 """
 
 import os
+import sys
 import time
 
 from datetime import datetime
 from datetime import timezone
+from logging import INFO
+from logging import basicConfig
+from logging import getLogger
 from pathlib import Path
 
 from lexicon.client import Client
@@ -23,6 +27,9 @@ DOMAIN_CACHE_BOUNCE_SECONDS = 180
 NAMECHEAP_MIN_TTL = 60
 WAIT_SECS = NAMECHEAP_MIN_TTL + 1
 CONFIG_PATH = Path(os.getenv("AUTH_HOOK_CONFIG_PATH", default="config/lexicon.yml"))
+LOG_FMT = "%(asctime)s %(levelname)s:%(message)s"
+basicConfig(format=LOG_FMT, level=INFO)
+LOG = getLogger(__name__)
 
 
 def debounce_domains(domain):
@@ -38,11 +45,11 @@ def debounce_domains(domain):
         now = datetime.now(tz=timezone.utc)
         diff_seconds = (now - mod_dt).seconds
         if diff_seconds < DOMAIN_CACHE_BOUNCE_SECONDS:
-            print("Checking for domain bouncing...")
+            LOG.info(f"Checking {domain} for domain bouncing...")
             with DOMAIN_CACHE_PATH.open("r") as cache_file:
                 old_domain = cache_file.read()
                 if old_domain == domain:
-                    print("Exiting to avoid domain bouncing")
+                    LOG.warning("Exiting to avoid domain bouncing")
                     exit(0)
 
     with DOMAIN_CACHE_PATH.open("w") as cache_file:
@@ -51,8 +58,10 @@ def debounce_domains(domain):
 
 def main():
     """Get arguments, host records, write the host records and sleep."""
+    LOG.info(f"Started {sys.argv[0]}")
     certbot_domain = os.getenv("CERTBOT_DOMAIN")
     debounce_domains(certbot_domain)
+    LOG.info(f"Updating TXT records for {certbot_domain}")
     certbot_validation = os.getenv("CERTBOT_VALIDATION")
 
     action = {
@@ -65,9 +74,9 @@ def main():
     config = ConfigResolver().with_config_file(str(CONFIG_PATH)).with_dict(action)
     Client(config).execute()
 
-    print(f"Waiting {WAIT_SECS}s for DNS to propagate...")
+    LOG.info(f"Waiting {WAIT_SECS}s for DNS to propagate...")
     time.sleep(WAIT_SECS)
-    print("Done.")
+    LOG.info("Done.")
 
 
 if __name__ == "__main__":
