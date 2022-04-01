8d478516028433697bf965cc2306aed59022d797
AJ Slater <aj@slater.net>
2022-04-01 12:03:48 -0700

    full tests

---
+++ test-full.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -x
+. ./auth-hook/config/test-env
+export CERTBOT_DOMAIN=bullfrog.sl8r.net
+export CERTBOT_VALIDATION=test-script-test-value
+cd auth-hook || exit 1
+if [ -n "$AUTH_HOOK_PROXY_DEST" ]; then
+  . ./tempproxy.sh
+fi
+poetry run ./lexicon_auth.py
