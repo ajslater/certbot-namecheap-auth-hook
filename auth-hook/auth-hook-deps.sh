b0714064762d3d41fee59c4fdd5a51be7744c29f
AJ Slater <aj@slater.net>
2022-04-01 13:15:44 -0700

    clearer packages for script

--- auth-hook/auth-hook-deps.sh
+++ auth-hook/auth-hook-deps.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 cd "$(dirname "$0")" || exit
-apk add --no-cache gcc musl-dev libffi-dev openssl-dev python3-dev openssh cargo
+apk add --no-cache \
+  cargo \
+  gcc \
+  libffi-dev \
+  musl-dev \
+  openssh \
+  openssl-dev \
+  python3-dev
 pip3 install -U pip
-CRYPTOGRAPHY_DONT_BUILD_RUST=1 pip3 install --no-cache-dir -U poetry
+pip3 install --no-cache-dir -U poetry
 poetry install --no-dev
