58e429463fbc9fb6b5770dc2254e9ac90ff5ce8c
AJ Slater <aj@slater.net>
2025-05-04 18:46:53 -0700

    update scripts

--- bin/lint-backend.sh
+++ bin/lint-backend.sh
@@ -11,14 +11,14 @@ npm run lint
 ###### Docker, Shell, Etc ######
 ################################
 if [ "$(uname)" = "Darwin" ]; then
-  # Hadolint & shfmt are difficult to install on linux
-  shellcheck disable=2035
-  hadolint ./*Dockerfile
-  shellharden ./**/*.sh
-  # subdirs aren't copied into docker builder
-  # .env files aren't copied into docker
-  shellcheck --external-sources ./**/*.sh
-  circleci config validate .circleci/config.yml
+    # Hadolint & shfmt are difficult to install on linux
+    shellcheck disable=2035
+    hadolint ./*Dockerfile
+    shellharden ./**/*.sh
+    # subdirs aren't copied into docker builder
+    # .env files aren't copied into docker
+    shellcheck --external-sources ./**/*.sh
+    circleci config validate .circleci/config.yml
 fi
 ./bin/roman.sh -i .prettierignore .
 codespell .
